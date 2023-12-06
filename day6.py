28c4c4fc56fa020db2436d1946ab004637598e8e
troingle <bugsinsidemyhead@gmail.com>
2023-12-06 17:53:28 +0200

    Day 6 complete

--- day6.py
+++ day6.py
@@ -1,26 +1,21 @@
-
-sum = 1
-allWays = []
+ways = 0
 
 with open('input.txt') as f:
     input = f.read().split(" ")
     
 times, distances = [int(input[8]), int(input[13]), int(input[18]), int(input[23].replace("\nDistance:", ""))], [int(input[26]), int(input[29]), int(input[32]), int(input[35])]
 
-#times, distances = [7, 15, 30], [9, 40, 200] # test input
+time, distance = "", ""
 
-print(times)
-print(distances)
+for n in range(len(times)):
+    time = str(time) + str(times[n])
+    distance = str(distance) + str(distances[n])
 
-for i in range(len(times)): # for each race
-    ways = 0
-    for n in range(times[i]): # calculate how many possibilities, n being the time we hold the button for AND our speed
-        if (times[i] - n) * n > distances[i]:
-            ways += 1
-    
-    allWays.append(ways)
+time, distance = int(time), int(distance)
 
-for w in allWays:
-    sum *= w
+for i in range(time):
+    if (time - i) * i > distance:
+        ways += 1
 
-print(sum)
+print(ways)
+
